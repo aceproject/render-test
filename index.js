9a75921c39f09ac49b6eb77082e1506e37758b3c
aceproject <yuichi.tsunoda@gmail.com>
2026-02-11 22:14:55 +0900

    add express server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Render!' });
+});
+
+app.listen(process.env.PORT || 3000);
+
