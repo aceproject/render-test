6894c3d3a469b994765c91e9ea5598f089e8db03
aceproject <yuichi.tsunoda@gmail.com>
2026-02-11 22:32:40 +0900

    update message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Render! ver.2' });
+  res.json({ message: `Hello ${process.env.APP_VERSION}` });
 });
 
 app.listen(process.env.PORT || 3000);
